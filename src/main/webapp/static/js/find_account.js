598d7a50752b0ad4af71ab02ab4724d65824653d
hellonuri <hellonuri.dvl@gmail.com>
2024-04-18 17:05:35 +0900

    아이디/비밀번호찾기.html 페이지 전환 완료

--- src/main/webapp/static/js/find_account.js
+++ src/main/webapp/static/js/find_account.js
@@ -3,14 +3,22 @@ function selectPage(e){
     
 }
 function updateSelectButton(buttonContent){
-    let idSelectPageBtn = document.querySelector("#changePage>a:first-child");
-    let pwdSelectPageBtn = document.querySelector("#changePage>a:last-child");
-    
+    const idSelectPageBtn = document.querySelector("#changePage>a:first-child");
+    const pwdSelectPageBtn = document.querySelector("#changePage>a:last-child");
+    const idFindPage = document.querySelector("#findIdPage");
+    const pwdFindPage = document.querySelector("#findPwdPage");
+
     if(buttonContent == idSelectPageBtn.text){
+        //id 찾기 페이지
         idSelectPageBtn.classList.add("selectPage");
         pwdSelectPageBtn.classList.remove("selectPage");
+        idFindPage.style.display = "grid";
+        pwdFindPage.style.display = "none";
     }else if(buttonContent == pwdSelectPageBtn.text){
+        //pw 찾기 페이지
         pwdSelectPageBtn.classList.add("selectPage");
         idSelectPageBtn.classList.remove("selectPage");
+        idFindPage.style.display = "none";
+        pwdFindPage.style.display = "grid";
     }
 }
