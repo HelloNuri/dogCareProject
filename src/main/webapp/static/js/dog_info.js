9608d4eda0a5e114d4c07943c1bcb47a5d124897
hellonuri <hellonuri.dvl@gmail.com>
2024-04-19 10:24:16 +0900

    stub function 바인딩 완료

--- src/main/webapp/static/js/dog_info.js
+++ src/main/webapp/static/js/dog_info.js
@@ -1,7 +1,25 @@
 function deleteSupply(e){
+    console.log("deleteSupply()");
     return false;
 }
 
 function insertSupply(e){
+    console.log("insertSupply()")
+    return false;
+}
+
+function submitDogInfo(e){
+    console.log("submitDogInfo()");
+    e.preventDefault();
+    // hidden type input도 채워야 함.
+}
+
+function fillDogs(){
+    console.log("fillDogs");
+    return false;
+}
+
+function updateSelectedDog(e){
+    console.log("updateSelectedDog");
     return false;
 }
