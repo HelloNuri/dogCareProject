b66d6808ae9831fd2b0e2f4e93ce301ca4918626
hellonuri <hellonuri.dvl@gmail.com>
2024-04-26 09:50:02 +0900

    Downgrade Dynamic Web module 5.0 into 3.1

--- src/dogCare/service/DogInformationService.java
+++ src/dogCare/service/DogInformationService.java
@@ -11,7 +11,7 @@ public interface DogInformationService {
 	public boolean addDogHealthReport(int dogId, DogInfoAddDTO dogInfo, Collection<SupplyAddDTO> supplies) throws SQLException;
 	
     public Map<Integer, Collection<Double>> getHealthGraphByDogId(int dogId);
-
+    
 	public Collection<Map<String, Double>> getSupplyStatistic(String breed, int startAge, int endAge, String category) throws SQLException;
-
+	
 }
