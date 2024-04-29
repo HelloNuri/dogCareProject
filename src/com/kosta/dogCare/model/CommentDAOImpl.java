56ead828bc74c9495b7533d12c3bb652390b8f62
hellonuri <hellonuri.dvl@gmail.com>
2024-04-29 10:40:06 +0900

    CommentDAO, DogDAO Singleton->CP

--- src/com/kosta/dogCare/model/CommentDAOImpl.java
+++ src/com/kosta/dogCare/model/CommentDAOImpl.java
@@ -8,28 +8,38 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 import com.kosta.dogCare.model.VO.CommentVO;
 
 public class CommentDAOImpl implements CommentDAO {
-	private Connection conn; 
+	DataSource dataSource;
 	
 	public CommentDAOImpl() throws ClassNotFoundException, SQLException {
-		conn = ConnectionManager.getConnection();
+		try {
+			Context context = new InitialContext();
+			dataSource =
+					(DataSource) context.lookup("java:comp/env/jdbc/myoracle");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
 	public boolean addComment(CommentVO comment) {
 		String sql = "Insert into comments (comment_id, comment_content, upload_time, album_id, user_id) values(comment_id_seq.nextval, ?,sysdate, ?, ?)";
 		boolean result = false;
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
 			pstmt.setString(1, comment.getContent());
 			pstmt.setInt(2, comment.getParentId());
 			pstmt.setString(3, comment.getUserId());
 			int rs = pstmt.executeUpdate();
 			if(rs == 1)
 				result = true;
-			pstmt.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -41,15 +51,14 @@ public class CommentDAOImpl implements CommentDAO {
 	public boolean editComment(CommentVO comment) {
 		String sql = "update comments set comment_content=? where comment_id = ? and user_id = ?";
 		boolean result = false;
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
 			pstmt.setString(1, comment.getContent());
 			pstmt.setInt(2, comment.getCommentId());
 			pstmt.setString(3, comment.getUserId());
 			int rs = pstmt.executeUpdate();
 			if(rs == 1)
 				result = true;
-			pstmt.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -60,15 +69,14 @@ public class CommentDAOImpl implements CommentDAO {
 	public boolean addRecomment(CommentVO recomment) {
 		String sql = "Insert into recomments (recomment_id, recomment_content, upload_time, comment_id, user_id) values(recomment_id_seq.nextval, ?,sysdate, ?, ?)";
 		boolean result = false;
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
 			pstmt.setString(1, recomment.getContent());
 			pstmt.setInt(2, recomment.getParentId());
 			pstmt.setString(3, recomment.getUserId());
 			int rs = pstmt.executeUpdate();
 			if(rs == 1)
 				result = true;
-			pstmt.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -79,15 +87,14 @@ public class CommentDAOImpl implements CommentDAO {
 	public boolean editRecomment(CommentVO recomment) {
 		String sql = "update recomments set recomment_content=? where recomment_id = ? and user_id = ?";
 		boolean result = false;
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
 			pstmt.setString(1, recomment.getContent());
 			pstmt.setInt(2, recomment.getCommentId());
 			pstmt.setString(3, recomment.getUserId());
 			int rs = pstmt.executeUpdate();
 			if(rs == 1)
 				result = true;
-			pstmt.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -98,14 +105,13 @@ public class CommentDAOImpl implements CommentDAO {
 	public boolean deleteComment(int commentId, String userId) {
 		String sql = "delete from comments where comment_Id = ? and user_id = ?";
 		boolean result = false;
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
 			pstmt.setInt(1, commentId);
 			pstmt.setString(2, userId);
 			int rs = pstmt.executeUpdate();
 			if(rs == 1)
 				result = true;
-			pstmt.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -116,14 +122,13 @@ public class CommentDAOImpl implements CommentDAO {
 	public boolean deleteRecomment(int recommentId, String userId) {
 		String sql = "delete from recomments where recomment_Id = ? and user_id = ?";
 		boolean result = false;
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
 			pstmt.setInt(1, recommentId);
 			pstmt.setString(2, userId);
 			int rs = pstmt.executeUpdate();
 			if(rs == 1)
 				result = true;
-			pstmt.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -135,16 +140,16 @@ public class CommentDAOImpl implements CommentDAO {
 		String sql = "select comment_id, comment_content, to_char(upload_time, 'YYYY-MM-DD HH24:MI:SS'), user_id from comments where album_id = ? order by upload_time";
 		Map<CommentVO, Collection<CommentVO>> result = new HashMap<>();
 		
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
 			pstmt.setInt(1, albumId);
-			ResultSet rs = pstmt.executeQuery();
-			CommentVO comment;
-			while(rs.next()) {
-				comment = new CommentVO(rs.getInt(1), rs.getString(2), rs.getTimestamp(3), albumId, rs.getString(4));
-				result.put(comment, getRecommentsByCommentId(comment.getCommentId()));
+			try(ResultSet rs = pstmt.executeQuery();){
+				CommentVO comment;
+				while(rs.next()) {
+					comment = new CommentVO(rs.getInt(1), rs.getString(2), rs.getTimestamp(3), albumId, rs.getString(4));
+					result.put(comment, getRecommentsByCommentId(comment.getCommentId()));
+				}
 			}
-			pstmt.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -155,14 +160,13 @@ public class CommentDAOImpl implements CommentDAO {
 		String sql = "select recomment_id, recomment_content, to_char(upload_time, 'YYYY-MM-DD HH24:MI:SS'), user_id from recomments where comment_id = ? order by upload_time";
 		Collection<CommentVO> result = new ArrayList<CommentVO>();
 		
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
 			pstmt.setInt(1, commentId);
-			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
-				result.add(new CommentVO(rs.getInt(1), rs.getString(2), rs.getTimestamp(3), commentId, rs.getString(4)));
+			try(ResultSet rs = pstmt.executeQuery();){
+				while(rs.next()) 
+					result.add(new CommentVO(rs.getInt(1), rs.getString(2), rs.getTimestamp(3), commentId, rs.getString(4)));
 			}
-			pstmt.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
--- src/com/kosta/dogCare/model/DogDAOImpl.java
+++ src/com/kosta/dogCare/model/DogDAOImpl.java
@@ -7,29 +7,37 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 import com.kosta.dogCare.model.VO.DogVO;
 
 public class DogDAOImpl implements DogDAO{
-	private Connection conn; 
+	DataSource dataSource;
 	
 	public DogDAOImpl() throws ClassNotFoundException, SQLException {
-//		Class.forName("oracle.jdbc.OracleDriver");
-//		String url = "jdbc:oracle:thin:@localhost:1521:XE";
-//		conn = DriverManager.getConnection(url, "hr", "hr");
-		conn = ConnectionManager.getConnection();
+		try {
+			Context context = new InitialContext();
+			dataSource =
+					(DataSource) context.lookup("java:comp/env/jdbc/myoracle");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
 	public int addDog(DogVO dog) {
 		String sql = "insert into dogs (dog_id, name, breed, birth_date, gender, neutrification, user_id) values (?, ?, ?, ?, ?, ?, ?)";
 		int dogId = -1;
-		try {
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				Statement stmt = conn.createStatement();
+				ResultSet rs =stmt.executeQuery("select dog_id_seq.nextval from dual");) {
 			//데이터 유효성 검사는 UI에서 한다고 가정한다.
-			Statement stmt = conn.createStatement();
-			ResultSet rs =stmt.executeQuery("select dog_id_seq.nextval from dual");
 			if(rs.next())
 				dogId = rs.getInt(1);
-			PreparedStatement pstmt = conn.prepareStatement(sql);
 			int i = 1;
 			pstmt.setInt(i++, dogId);
 			pstmt.setString(i++, dog.getName());
@@ -39,9 +47,6 @@ public class DogDAOImpl implements DogDAO{
 			pstmt.setString(i++, "" + dog.getNeutrification());
 			pstmt.setString(i++, dog.getUserId());
 			pstmt.executeUpdate();
-			pstmt.close();
-			rs.close();
-			stmt.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -52,16 +57,14 @@ public class DogDAOImpl implements DogDAO{
 	public Collection<DogVO> getDogsByUserId(String userId) {
 		String sql = "select dog_id, name, breed, birth_date, gender, neutrification from dogs where user_id = ?";
 		Collection<DogVO> result = new ArrayList<DogVO>();
-		try {
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
 			//데이터 유효성 검사는 UI에서 한다고 가정한다.
-			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, userId);
-			ResultSet rs = pstmt.executeQuery();
-			while (rs.next()) {
-				result.add(new DogVO(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getDate(4), rs.getString(5).charAt(0), rs.getString(6).charAt(0), userId));
+			try(ResultSet rs = pstmt.executeQuery();){
+				while (rs.next()) 
+					result.add(new DogVO(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getDate(4), rs.getString(5).charAt(0), rs.getString(6).charAt(0), userId));
 			}
-			pstmt.close();
-			rs.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -72,14 +75,13 @@ public class DogDAOImpl implements DogDAO{
 	public String getBreedByDogId(int dogId) {
 		String sql = "select breed from dogs where dog_id = ?";
 		String breed = null;
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
 			pstmt.setInt(1, dogId);
-			ResultSet rs = pstmt.executeQuery();
-			if(rs.next())
-				breed = rs.getString(1);
-			pstmt.close();
-			rs.close();
+			try(ResultSet rs = pstmt.executeQuery();){
+				if(rs.next())
+					breed = rs.getString(1);
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -90,14 +92,13 @@ public class DogDAOImpl implements DogDAO{
 	public DogVO getDog(int dogId) {
 		String sql = "select name, breed, birth_date, gender, neutrification, user_id from dogs where dog_id = ?";
 		DogVO dog = null;
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);) {
 			pstmt.setInt(1, dogId);
-			ResultSet rs = pstmt.executeQuery();
-			if(rs.next())
-				dog = new DogVO(dogId, rs.getString(1), rs.getString(2), rs.getDate(3), rs.getString(4).charAt(0), rs.getString(5).charAt(0), rs.getString(6));
-			pstmt.close();
-			rs.close();
+			try(ResultSet rs = pstmt.executeQuery();){
+				if(rs.next())
+					dog = new DogVO(dogId, rs.getString(1), rs.getString(2), rs.getDate(3), rs.getString(4).charAt(0), rs.getString(5).charAt(0), rs.getString(6));
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
