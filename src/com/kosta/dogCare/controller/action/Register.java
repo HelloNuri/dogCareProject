679cd6f1b736d008d90e458112d7c4e10476a484
hellonuri <hellonuri.dvl@gmail.com>
2024-04-29 15:20:47 +0900

    register 백엔드 완성

--- src/com/kosta/dogCare/controller/action/Register.java
+++ src/com/kosta/dogCare/controller/action/Register.java
@@ -1,9 +1,11 @@
 package com.kosta.dogCare.controller.action;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import com.kosta.dogCare.Service.RegistrationServiceImpl;
 import com.kosta.dogCare.model.VO.UserVO;
@@ -12,18 +14,41 @@ public class Register implements Action {
 
 	@Override
 	public String execute(HttpServletRequest request) throws ServletException, IOException {
-		String name     = request.getParameter("name");
 		String userId   = request.getParameter("id");
-		String pwd      = request.getParameter("password");
 		String nickname = request.getParameter("nickname");
+		String name     = request.getParameter("name");
+		String pwd      = request.getParameter("password");
 		String email    = request.getParameter("email");
-		String valid    = request.getParameter("valid");
 		String url = "controller?cmd=registerUI";
+		
 		UserVO user = new UserVO(userId, nickname, name, pwd, email);
 		
-		if(new RegistrationServiceImpl().register(user))
+		if(isCorrectValidationNumber(request) && new RegistrationServiceImpl().register(user)){
 			url = "controller?cmd=loginUI";
+		}
 		return url;
+	}
+
+	private boolean isCorrectValidationNumber(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		String numInputName = "vNumber";
+		String    numSessionAttr = "vNumber";
+		String maxAgeSessionAttr = "vNumberMaxAge";
+		
+		String actualNum = request.getParameter(numInputName);
+		String expectedNum   = (String)        session.getAttribute(numSessionAttr);
+		LocalDateTime maxAge = (LocalDateTime) session.getAttribute(maxAgeSessionAttr);
+		
+		if(expectedNum == null || maxAge.isAfter(LocalDateTime.now()))
+			return false;	
+		
+		if(actualNum.equals(expectedNum)){
+			session.removeAttribute(numSessionAttr);
+			session.removeAttribute(maxAgeSessionAttr);
+			return true;
+		}
+		
+		return false;//쓸일없음
 	}
 
 }
--- src/com/kosta/dogCare/controller/action/SendValidationNum.java
+++ src/com/kosta/dogCare/controller/action/SendValidationNum.java
@@ -1,16 +1,33 @@
 package com.kosta.dogCare.controller.action;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 public class SendValidationNum implements Action {
-
 	@Override
 	public String execute(HttpServletRequest request) throws ServletException, IOException {
-		String url = "static/json/???.json";
+		String url = "static/json/false.jsp";
+		int maxAgeMins = 3;
+		
+		String vNumber = createVNumber(); 
+		LocalDateTime expiredDate = LocalDateTime.now().plusMinutes(maxAgeMins);
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("vNumber"      , vNumber);
+		session.setAttribute("vNumberMaxAge", expiredDate);
+		
+		url = "static/json/true.jsp";
 		return url;
+	}
+
+	private String createVNumber() {
+		int vNumber = (int)(Math.random() * 9000) + 1000;//1000~9999 사이의 수 생성
+		System.out.println(vNumber);
+		return vNumber + "";
 	}
 
 }
